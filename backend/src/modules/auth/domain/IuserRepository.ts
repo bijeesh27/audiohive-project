ad3820460d44ec46d7368349145368ffac994140
bijeesh27 <bijeeshbiju3669@gmail.com>
2026-07-17 10:44:27 +0530

    feat(auth): add forgot password and change password functionality

--- backend/src/modules/auth/domain/IuserRepository.ts
+++ backend/src/modules/auth/domain/IuserRepository.ts
@@ -4,4 +4,8 @@ export interface IuserRepository {
   findByEmail(email: string): Promise<IuserDocument | null>;
   createUser(data: IuserDocument): Promise<void>;
   deteleUser(email: string): Promise<void>;
+  updateUser(
+    userId: string,
+    data: Partial<IuserDocument>,
+  ): Promise<IuserDocument>;
 }
--- backend/src/modules/auth/presentation/auth.controller.ts
+++ backend/src/modules/auth/presentation/auth.controller.ts
@@ -5,12 +5,16 @@ import { OtpUseCase } from "../application/usecases/otpUseCase.ts";
 import { LoginUserUseCase } from "../application/usecases/LoginUserUseCase.ts";
 import jwt from "jsonwebtoken";
 import { ApiResposne } from "../../../common/Response/Response.ts";
+import { ForgetUseCase } from "../application/usecases/ForgetUseCase.ts";
+import { ChangePasswordUseCase } from "../application/usecases/changePasswordUseCase.ts";
 
 export class AuthController {
   constructor(
     private readonly registerUserUseCase: RegiterUserUseCase,
     private readonly otpUseCase: OtpUseCase,
     private readonly loginUserUseCase: LoginUserUseCase,
+    private readonly forgetUseCase:ForgetUseCase,
+    private readonly changePasswordUseCase:ChangePasswordUseCase
   ) {}
   async register(req: Request, res: Response, next: NextFunction) {
     try {
@@ -22,8 +26,8 @@ export class AuthController {
   }
   async verifyOtp(req: Request, res: Response, next: NextFunction) {
     try {
-      await this.otpUseCase.execute(req.body);
-      return ApiResposne.success(res,'otp-verified suceesfully',)
+      let userData=await this.otpUseCase.execute(req.body);
+      return ApiResposne.success(res,'otp-verified suceesfully',userData)
     } catch (error) {
       next(error);
     }
@@ -40,6 +44,23 @@ export class AuthController {
       return ApiResposne.success(res,'login successfully',{accessToken})
     } catch (error) {
       next(error);
+    }
+  }
+  async verifyEmail(req: Request, res: Response, next: NextFunction){
+    try {
+        const user=await this.forgetUseCase.excute(req.body)
+        return ApiResposne.success(res,'email verified',user)
+    } catch (error) {
+        next(error)
+    }
+  }
+  async changePassword(req: Request, res: Response, next: NextFunction){
+    try {
+      const email=req.params.email
+     
+      await this.changePasswordUseCase.execute(req.body,email)
+    } catch (error) {
+      next(error)
     }
   }
 }
